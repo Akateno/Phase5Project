43ec34c9249d9b7e7079243ee4d9b110773ede7a
Erick Vargas <erickkvargas@gmail.com>
2022-07-18 13:01:56 -0700

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
-  resources :likes, only: [:create, :destroy, :index]
-  resources :user_books
-  resources :recommendations do
-    resources :comments
-    
-  end 
+ 
+      resources :likes, only: [:create, :destroy, :index]
+      resources :user_books
+      resources :recommendations do
+        resources :comments
+        
+      end 
 
-  resources :books
-  resources :users
-  
-  #session controller
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+      resources :books
+      resources :users
+      
+      #session controller
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
 
-  #user controller 
-  post "/signup", to: "users#create"
-  get "/me", to: "users#show"
+      #user controller 
+      post "/signup", to: "users#create"
+      get "/me", to: "users#show"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
